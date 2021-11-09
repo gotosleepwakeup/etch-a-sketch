406bec68e0e335d8225c7a8b68d4f86709a2bde3
Dustin Bauder <dbauder66@gmail.com>
2021-11-09 08:46:36 -0700

    connected number to preview grid

--- js/script.js
+++ js/script.js
@@ -1,56 +1,84 @@
 // query selectors
 let wrapper = document.querySelector('#wrapper');
 let dialogContent = document.querySelector('#dialog-content');
+let rows = [];
+let squares = [];
 
 function hoverSquare() {
     this.classList.add('square-hover');
 }
 
-for (let i = 0; i < 16; i++) {
+function createGrid(resolution) {
+    wrapper.innerHTML = '';
+    for (let i = 0; i < resolution; i++) {
 
-    // creates 16 rows
-    let row = document.createElement('div');
-    row.className = 'row';
-    wrapper.appendChild(row);
-
-    // creates 16 squares for each row
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement('div');
-        square.className = 'square';
-        row.appendChild(square);
-
-        // listens to hover on the squares
-        square.addEventListener('mouseover', hoverSquare);
+        // creates rows
+        let row = document.createElement('div');
+        row.className = 'row';
+        wrapper.appendChild(row);
+        rows.push(row);
+    
+        // creates squares for each row
+        for (let j = 0; j < resolution; j++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+            row.appendChild(square);
+            squares.push(square);
+    
+            // listens to hover on the squares
+            square.addEventListener('mouseover', hoverSquare);
+        }
     }
+}
+createGrid(1);
+
+
+console.log(rows);
+console.log(squares);
+
+function randomizeSquares() {
+    squares.forEach(square => {
+        if (Math.random() < 0.5) {
+            square.classList.add('square-hover');
+        } else {
+            square.classList.remove('square-hover');
+        }
+    });
 }
 
 let outputNum = "";
 function writeNumber(e) {
-    // TODO: maximum 100
     // TODO: animate numbers in
-    // TODO: backspace to remove a number
     // TODO: flashy animation on grid as a preview of grid size
     let inputNum = +e.key;
 
+    // disallow leading zeros
     if (outputNum.length == 0 && inputNum == 0) {
         return;
     }
 
+    // restrict numbers to only 0-9
     if (inputNum >= 0 && inputNum <= 9) {
-        
-            if (outputNum.length < 3) {
-                let holdNum = outputNum;
-                outputNum = outputNum + e.key;
-                if (+outputNum > 100) {
-                    console.log('NO');
-                    outputNum = holdNum;
-                    return;
-                } else {
-                    dialogContent.textContent = outputNum;
-                    dialogContent.classList.add("dialog-large-numbers");
-                }
+
+        // disallow length greater than 3
+        if (outputNum.length < 3) {
+            let holdNum = outputNum;
+            outputNum = outputNum + e.key;
+
+            // disallow numbers over 100
+            if (+outputNum > 100) {
+                outputNum = holdNum;
+                return;
+            } else {
+
+                // write the number
+                dialogContent.textContent = outputNum;
+                dialogContent.classList.add("dialog-large-numbers");
+                createGrid(+outputNum);
+                randomizeSquares();
             }
-        
+        }
+
     }
 
     if (e.key == "Backspace" && outputNum.length != 0) {
